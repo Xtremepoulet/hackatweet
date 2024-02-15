a81a8a2821eeb6c8656b2dfc9eac99c955b6ca9a
yanis <layrac.yanis@gmail.com>
2024-02-15 18:34:23 +0100

    commiting changes

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 const userRouter = require('./routes/users');
+const tweetRouter = require('./routes/tweet');
 
 var app = express();
 const cors = require('cors')
@@ -21,5 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', userRouter);
+app.use('/tweet', tweetRouter);
 
 module.exports = app;
--- backend/models/tweet.js
+++ backend/models/tweet.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 
 const tweetSchema = mongoose.Schema({
-    firstname: String,
-    username: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
     date: Date,
     message: String,
     likeCount: Number,
