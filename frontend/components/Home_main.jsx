fe8d43fe0ab4ce7e6dc5a4de7a9d8859ce4a2cbc
yanis <layrac.yanis@gmail.com>
2024-02-15 13:07:09 +0100

    changing frontend

---
+++ frontend/components/Home_main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home_main = () => {
+    return(
+        <div>
+            <p>WE ARE IN SOLDIER</p>
+        </div>
+    );
+}
+
+
+
+export default Home_main;
